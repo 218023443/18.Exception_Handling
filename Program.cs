28f7ee5c9831178c202eba5d125dfec6b5367342
A252557 <A252557@LSBSA-3WCCHW3>
2023-12-06 15:53:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter an number ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter another number ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Equals:  ");
+                Console.WriteLine(num1 / num2);
+
+
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.Message);
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
